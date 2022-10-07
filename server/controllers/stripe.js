f0d35ee5a30149aad3db40b70fb6a8dad2b1e4c8
Ayush <aayusharora4444@gmail.com>
2022-10-07 15:09:04 +0530

    stripe setup

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -1,7 +1,21 @@
 import User from "../models/user";
+import Stripe from 'stripe';
+
+const stripe = Stripe(process.env.STRIPE_SECRET);
 
 export const createConnectAccount = async(req,res) =>{
 
+// find user from db
 const user = await User.findById(req.auth._id).exec();
 console.log("user ==>",user);
+
+
+// if user don't have stripe id create now
+
+const account = await stripe.accounts.create({
+    type: "express",
+});
+
+console.log("Account--",account);
+
 }
