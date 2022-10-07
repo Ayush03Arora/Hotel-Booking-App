902b726df400ba46fb3fa21418736ef6de3089bb
Ayush <aayusharora4444@gmail.com>
2022-10-07 20:02:30 +0530

    commit

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -1,6 +1,6 @@
 import User from "../models/user";
 import Stripe from 'stripe';
-
+import queryString from 'query-string';
 const stripe = Stripe(process.env.STRIPE_SECRET);
 
 export const createConnectAccount = async(req,res) =>{
@@ -11,11 +11,20 @@ console.log("user ==>",user);
 
 
 // if user don't have stripe id create now
-
+if(!user.stripe_account_id)
+{
 const account = await stripe.accounts.create({
     type: "express",
 });
 
 console.log("Account--",account);
-
+user.stripe_account_id = account.id;
+user.save();
 }
+
+// create login link
+
+let accountLink = await stripe.accountLinks.create()
+
+
+};
